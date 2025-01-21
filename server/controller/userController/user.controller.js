618bb68abd822e070f1f24dacb95ab57378d299f
shyam1998 <shyamsundarsahoo1998@gmail.com>
2025-01-21 23:40:05 +0530

    login signup userdata api created

--- server/controller/userController/user.controller.js
+++ server/controller/userController/user.controller.js
@@ -1,31 +1,79 @@
 const express = require("express");
 const { sendToken } = require("../../utils/feature");
 const TryCatch = require("../../utils/TryCatch");
-const { User } = require("../../models")
+const { User } = require("../../models");
+const AppError = require("../../utils/appError");
+const bcrypt = require("bcryptjs");
 
-const register = TryCatch(async (req, res) => {
+const register = TryCatch(async (req, res, next) => {
     try {
-        
-        const { name, username, password, avatar } = req.body;
+        let { name, username, password, avatar } = req.body;
         // console.log("User model", User)
         // console.log("register controller");
-        const newUser = User.create({ ...req.body });
-        sendToken(res, 201, newUser, "Signup sucessfully");
+        //password hash
+        const saltRound = 10;
+        const hashPassword = await bcrypt.hash(password, saltRound);
+        password = hashPassword;
+
+        const existingUser = await User.findOne({ where: { name } });
+        if (existingUser) {
+            return next(new AppError('User with this name already exists', 400));
+        }
+
+        const newUser = User.create({ name, username, password, avatar });
+
+        if (newUser) {
+            sendToken(res, 201, newUser, "Signup sucessfully");
+        } else {
+            return next(new AppError('Registration failed', 400));
+        }
     }
     catch (error) {
         console.log(`Error in register controller ${error}`)
     }
 })
 
-const login = (req, res) => {
-    try {
-        res.send("hello world")
+const login = TryCatch(async (req, res) => {
+    const { username, password } = req.body;
 
-        sendToken(res, 201, { id: 1 }, "login sucessfully");
+    const userExist = await User.findOne({ where: { username } });
+    if (userExist) {
+
+      // Retrieve the hashed password from the database
+      const hashedPassword = userExist.rows[0].password;
+      const passwordCheck = await bcrypt.compare(password, hashedPassword);
+    
+      const userstorepassword = emailExist.dataValues.password;
+    //const passwordCheck = userstorepassword == password
+
+      if (passwordCheck) {
+        return sendToken(res, 201, newUser, "Login sucessfully");
+      }
+      else {
+        return next(new AppError('Invalid Credentials', 400))
+      }
+
     }
-    catch (error) {
-
+    else {
+      return next(new AppError('Invalid Credentials', 400))
     }
-}
+    
+})
 
-module.exports = { login, register }
+const getDetails = TryCatch(async (req, res, next) => {
+    const { id } = req.user;
+
+    if (!id) {
+        return next(new AppError("Id not found", 404))
+    }
+
+    const user = await User.findOne({ where: { id } });
+
+    if (!user) {
+        return next(new AppError("User not found", 404))
+    }
+
+    res.status(200).json({ success: true, data: user })
+})
+
+module.exports = { login, register, getDetails }
--- server/router/user.routes.js
+++ server/router/user.routes.js
@@ -1,10 +1,17 @@
 const express  = require("express");
-const { login, register } = require("../controller/userController/user.controller");
+const { login, register, getDetails } = require("../controller/userController/user.controller");
 const { singleAvatar } = require("../middleware/multer");
+const { isAuthenticated } = require("../middleware/auth");
+
+const app = express();
 
 const userRoute = express.Router();
 
 userRoute.post("/register", singleAvatar, register)
 userRoute.get("/login", login)
+
+app.use(isAuthenticated);
+
+userRoute.get("/me", getDetails)
 
 module.exports = userRoute
