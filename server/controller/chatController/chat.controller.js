bf575820624e9a6615e51a1cc83d00cde60583c7
shyam1998 <shyamsundarsahoo1998@gmail.com>
2025-02-26 23:09:26 +0530

    update

--- server/controller/chatController/chat.controller.js
+++ server/controller/chatController/chat.controller.js
@@ -46,7 +46,7 @@ const getMyChats = TryCatch(async (req, res, next) => {
     );
 
     const uniqueMemberIds = [...new Set(chats.flatMap(chat => chat.member))];
-    
+
 
     const users = await User.findAll({
         where: {
@@ -68,14 +68,14 @@ const getMyChats = TryCatch(async (req, res, next) => {
 
     const transformedChats = chatsWithUsers.map(({ id, name, member, groupChat }) => {
         const otherMember = getOtherMember(member, req.user.id);
-        
+
         return {
             id,
             groupChat,
             avatar: groupChat ? member.slice(0, 3).flatMap(({ avatar }) => avatar.map(item => item.url)) : [otherMember.avatar.url],
             name: groupChat ? name : otherMember.name,
-            members: member.reduce((prev, curr)=>{
-                if(curr.id.toString() !== req.user.id.toString()){
+            members: member.reduce((prev, curr) => {
+                if (curr.id.toString() !== req.user.id.toString()) {
                     prev.push(curr.id)
                 }
 
@@ -93,7 +93,62 @@ const getMyChats = TryCatch(async (req, res, next) => {
 
 });
 
+const getMyGroups = TryCatch(async (req, res, next) => {
+    const { id } = req.user;
+    const chats = await Chat.findAll({
+        where: {
+            // creator: id,
+            groupChat: true,
+            member: {
+                [Op.contains]: [id.toString()],
+            },
+        },
+    },
+    );
+
+
+    const uniqueMemberIds = [...new Set(chats.flatMap(chat => chat.member))];
+
+
+    const users = await User.findAll({
+        where: {
+            id: {
+                [Op.in]: uniqueMemberIds.map(id => parseInt(id)), // Convert back to integers
+            },
+        },
+        attributes: ["id", "name", "avatar"],
+    });
+
+    const chatsWithUsers = chats.map(chat => {
+        const chatData = chat.toJSON();
+        chatData.member = chatData.member.map(memberId => {
+            const user = users.find(u => u.id === parseInt(memberId));
+            return user ? { id: user.id, name: user.name, avatar: user.avatar } : null;
+        }).filter(Boolean);
+        return chatData;
+    });
+
+
+
+    const transformedGroups = chatsWithUsers.map(({ id, name, member, groupChat }) => ({
+        id,
+        groupChat,
+        avatar: member.slice(0, 3).flatMap(({ avatar }) =>
+            Array.isArray(avatar) ? avatar.map(item => item.url) : []
+        ),
+        name,
+    }));
+
+    res.status(200).json({
+        success: true,
+        chats: transformedGroups,
+    });
+
+
+});
+
 module.exports = {
     newGroup,
     getMyChats,
+    getMyGroups,
 }
--- server/router/chat.routes.js
+++ server/router/chat.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { singleAvatar } = require("../middleware/multer");
 const { isAuthenticated } = require("../middleware/auth");
-const { newGroup, getMyChats } = require("../controller/chatController/chat.controller");
+const { newGroup, getMyChats, getMyGroups } = require("../controller/chatController/chat.controller");
 
 const chatRoute = express.Router();
 
 chatRoute.post("/group", isAuthenticated, newGroup);
 chatRoute.get("/my", isAuthenticated, getMyChats);
+chatRoute.get("/my/groups", isAuthenticated, getMyGroups);
 
 module.exports = chatRoute
